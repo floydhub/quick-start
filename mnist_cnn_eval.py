afab8f4d198396c4d9c0a2706e8c4e0edcb5467b
McKay Ward <mckay@mckayward.com>
2017-09-27 12:29:03 -0500

    remove old files

--- mnist_cnn_eval.py
+++ mnist_cnn_eval.py
@@ -15,110 +15,15 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/mnist", one_hot=True)
 
-# Parameters
-learning_rate = 0.001
-training_iters = 200000
-batch_size = 128
-display_step = 10
-
-# Network Parameters
-n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
-dropout = 0.75 # Dropout, probability to keep units
-
-# tf Graph input
-x = tf.placeholder(tf.float32, [None, n_input])
-y = tf.placeholder(tf.float32, [None, n_classes])
-keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
-
-
-# Create some wrappers for simplicity
-def conv2d(x, W, b, strides=1):
- # Conv2D wrapper, with bias and relu activation
- x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
- x = tf.nn.bias_add(x, b)
- return tf.nn.relu(x)
-
-
-def maxpool2d(x, k=2):
- # MaxPool2D wrapper
- return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                       padding='SAME')
-
-
-# Create model
-def conv_net(x, weights, biases, dropout):
- # Reshape input picture
- x = tf.reshape(x, shape=[-1, 28, 28, 1])
-
- # Convolution Layer
- conv1 = conv2d(x, weights['wc1'], biases['bc1'])
- # Max Pooling (down-sampling)
- conv1 = maxpool2d(conv1, k=2)
-
- # Convolution Layer
- conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
- # Max Pooling (down-sampling)
- conv2 = maxpool2d(conv2, k=2)
-
- # Fully connected layer
- # Reshape conv2 output to fit fully connected layer input
- fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
- fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
- fc1 = tf.nn.relu(fc1)
- # Apply Dropout
- fc1 = tf.nn.dropout(fc1, dropout)
-
- # Output, class prediction
- out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
- return out
-
- # Store layers weight & bias
-weights = {
-     # 5x5 conv, 1 input, 32 outputs
-     'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-     # 5x5 conv, 32 inputs, 64 outputs
-     'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-     # fully connected, 7*7*64 inputs, 1024 outputs
-     'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
-     # 1024 inputs, 10 outputs (class prediction)
-     'out': tf.Variable(tf.random_normal([1024, n_classes]))
- }
-
-biases = {
-     'bc1': tf.Variable(tf.random_normal([32])),
-     'bc2': tf.Variable(tf.random_normal([64])),
-     'bd1': tf.Variable(tf.random_normal([1024])),
-     'out': tf.Variable(tf.random_normal([n_classes]))
- }
-
-#  # Construct model
-pred = conv_net(x, weights, biases, keep_prob)
-#
-#  # Define loss and optimizer
-#  cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-#  optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-#
-# Evaluate model
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-#
-
-# Initializing the variables
-init = tf.global_variables_initializer()
-
-# FIX: Add ops to save and restore all the variables.
-saver = tf.train.Saver()
-
 # Launch the graph
 with tf.Session() as sess:
-    sess.run(init)
-    # saver = tf.train.import_meta_graph('/output/mnist_model.meta')
-    # saver.restore(sess,tf.train.latest_checkpoint('/output'))
-
-    # Restore model weights from previously saved model
-    saver.restore(sess, "/output/mnist_model")
-    print("Model restored from file: %s" % "/output/mnist_model")
+    tf.saved_model.loader.load(sess, ["EVALUATING"], "/model/cnn_model")
+    # Retrieve placeholder tensor by name
+    graph = tf.get_default_graph()
+    x = graph.get_tensor_by_name("x:0")
+    y = graph.get_tensor_by_name("y:0")
+    accuracy = graph.get_tensor_by_name("accuracy:0")
+    keep_prob = graph.get_tensor_by_name("keep_prob:0")
 
     # Calculate accuracy for 256 mnist test images
     print("Testing Accuracy:", \
